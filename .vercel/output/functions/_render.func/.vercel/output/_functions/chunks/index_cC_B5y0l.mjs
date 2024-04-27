a6cc0dac3f8f4839588e3077442036f23fa57d91
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-04-27 15:38:44 +0200

    test

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_cC_B5y0l.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Bpjy-96G.mjs').then(n => n.a);
-
-export { page };
--- .vercel/output/static/_astro/hoisted.CU5BQE6O.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./cart.BFHKmjat.js";const c=()=>{const e=t.get().reduce((n,a)=>n+a.quantity,0);document.getElementById("cart-quantity").textContent=e.toString()};t.subscribe(c);
